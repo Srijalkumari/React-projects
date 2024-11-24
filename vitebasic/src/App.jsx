b0b5ff4773d55546605eb4a110f48099a29c089c
Srijalkumari <srijal2004@gmail.com>
2024-11-24 17:09:05 +0530

    Add new file(s)

--- vitebasic/src/App.jsx
+++ vitebasic/src/App.jsx
@@ -1,12 +1,21 @@
-
+import Chai from './practice.jsx'
 
 function App() {
  
 
   return (
-    <h1>
-      Hello Everyone!!!
-    </h1>
+    /*
+    rule of jsx: you can only return 1 tag
+    <> </>  is used to return more than one tag in jsx
+    fundamentaly it is considered only one tag
+    <> is called fragment in react
+    */
+    <>               
+      <Chai/>
+      <h2>hello!!</h2>
+      <p>this is a practice code</p>
+   
+   </>
   )
 }
 
